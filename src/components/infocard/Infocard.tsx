393c90273a6bb982592d1be876803318bb1e25b6
Manu <pflaume1337@gmail.com>
2025-04-01 15:06:07 +0200

    Portfolio fortgeführt

--- src/components/infocard/Infocard.tsx
+++ src/components/infocard/Infocard.tsx
@@ -1,24 +1,35 @@
 'use client';
 import { useEffect, useState } from "react";
-import Navbar from "../navbar/Navbar";
-import Sidenav from "../sidenav/Sidenav";
-import { useUser } from "@/app/context/UserContext";
-import Head from 'next/head';
-import styles from "./Infocard.module.css";
 import { useRouter } from "next/navigation";
 
-export default function Portfolio() {
+import { useUser } from "@/app/context/UserContext";
 
-    const { isLoggedIn } = useUser();
+import Navbar from "../navbar/Navbar";
+import Sidenav from "../sidenav/Sidenav";
+
+import styles from "./Infocard.module.css";
+
+import { FaTimeline } from "react-icons/fa6";
+import { GiTicTacToe } from "react-icons/gi";
+import { BiLogoSpringBoot } from "react-icons/bi";
+
+import Link from "next/link";
+import { FaReact } from "react-icons/fa";
+import { VscSnake } from "react-icons/vsc";
+
+
+export default function Portfolio() {
     const router = useRouter();
     const [isSidenavOpen, setIsSidenavOpen] = useState<boolean>(false);
 
+ /*
+ fonts
+ blau mittig
+ blau trenner?
+ sidenav anpassen
+ navbar anpassen
+ */
 
-    useEffect(() => {
-        if (!isLoggedIn) {
-            router.push("/login");
-        }
-    }, []);
 
     return (
         <>
@@ -33,19 +44,19 @@ export default function Portfolio() {
                     <h2 className={styles.sectionTitle}>About Me</h2>
                     <p className={styles.aboutText}>
                         Willkommen auf meinem Portfolio! Ich bin ein angehender Softwareentwickler mit einem starken Fokus auf
-                        Full-Stack-Entwicklung. Hier finden Sie eine Übersicht über die Projekte, die ich bereits realisieren konnte und außerdem 
+                        Full-Stack-Entwicklung. Hier finden Sie eine Übersicht über die Projekte, die ich bereits realisieren konnte und außerdem
                         das Wissen, das ich mir dadurch aneignen konnte.
                     </p>
                 </section>
 
-                    <h2 className={styles.sectionTitle}>Technical Skills</h2>
+                <h2 className={styles.sectionTitle}>Technical Skills</h2>
                 <section className={styles.skillsSection}>
                     <div className={styles.skillCategory}>
                         <h3 className={styles.skillCategoryTitle}>Languages</h3>
                         <ul className={styles.skillsList}>
-                            <li className={styles.skillItem}>JavaScript/TypeScript</li>
+                            <li className={styles.skillItem}>JavaScript / TypeScript</li>
                             <li className={styles.skillItem}>Java / Objektorientierte Programmierung</li>
-                            <li className={styles.skillItem}>HTML/CSS</li>
+                            <li className={styles.skillItem}>HTML / CSS</li>
                             <li className={styles.skillItem}>SQL</li>
                         </ul>
                     </div>
@@ -62,7 +73,7 @@ export default function Portfolio() {
                     <div className={styles.skillCategory}>
                         <h3 className={styles.skillCategoryTitle}>Tools & Platforms</h3>
                         <ul className={styles.skillsList}>
-                            <li className={styles.skillItem}>Git/GitHub</li>
+                            <li className={styles.skillItem}>Git / GitHub</li>
                             <li className={styles.skillItem}>RESTful APIs</li>
                             <li className={styles.skillItem}>Docker</li>
                             <li className={styles.skillItem}>MySQL</li>
@@ -74,112 +85,138 @@ export default function Portfolio() {
                     <h2 className={styles.sectionTitle}>Featured Projects</h2>
 
                     <div className={styles.project}>
-                        <h3 className={styles.projectTitle}>1. AskGPT Chrome Extension</h3>
-                        <div className={styles.projectLink}>
-                            <a href="https://github.com/manuelpalluotto/AskGPT-Chrome-Extension" target="_blank" rel="noopener noreferrer">
-                                GitHub Repository
-                            </a>
+                        <div style={{ margin: "0.2rem" }}>
+                            <Link href="https://github.com/manuelpalluotto/TicTacToe" target="_blank" rel="noopener noreferrer"><GiTicTacToe size={100} color={"#0f0f0f"} /></Link>
+                            <div className={styles.projectLink}>
+                                <span style={{ backgroundColor: "var(--youtubeFont)", color: "var(--ytBackground)", fontSize: "0.8rem" }}>GitHub Repository</span>
+                            </div>
+                            <h3 className={styles.projectTitle}>Tic Tac Toe</h3>
                         </div>
                         <p className={styles.projectDescription}>
-                            A Chrome extension that integrates OpenAI's API to bring ChatGPT capabilities directly into your browser.
+                            Meine erste Java-Anwendung: Realisierung des Spiels "Tic Tac Toe".
+                            Statt als Konsolenanwendung wurde es mit<br />grafischer Benutzeroberfläche implementiert.
                         </p>
                         <div className={styles.projectFeatures}>
-                            <h4 className={styles.featuresTitle}>Key Features:</h4>
+                            <h4 className={styles.featuresTitle}>Zusammenfassung:</h4>
                             <ul className={styles.featuresList}>
-                                <li className={styles.featureItem}>Direct integration with OpenAI's ChatGPT API</li>
-                                <li className={styles.featureItem}>Customizable interface for user interactions</li>
-                                <li className={styles.featureItem}>Context-aware assistance based on browser content</li>
-                                <li className={styles.featureItem}>User-friendly popup interface</li>
+                                <li className={styles.featureItem}>Grafische Oberfläche dank der Java Swing Bibliothek (JFrame etc.)</li>
+                                <li className={styles.featureItem}>Verschachtelte For-Loops</li>
+                                <li className={styles.featureItem}>Erstellen und Einsetzen eigener Methoden</li>
                             </ul>
                         </div>
                         <div className={styles.projectTech}>
-                            <h4 className={styles.techTitle}>Technologies Used:</h4>
-                            <p className={styles.techText}>JavaScript, HTML/CSS, Chrome Extension API, OpenAI API</p>
+                            <h4 className={styles.techTitle}>Genutzte Technologien:</h4>
+                            <p className={styles.techText}>Java, Java Swing Bibliothek</p>
                         </div>
                     </div>
 
                     <div className={styles.project}>
-                        <h3 className={styles.projectTitle}>2. PancakeSwap Price Bot</h3>
-                        <div className={styles.projectLink}>
-                            <a href="https://github.com/manuelpalluotto/PancakeSwapPriceBot" target="_blank" rel="noopener noreferrer">
-                                GitHub Repository
-                            </a>
+                        <div style={{ margin: "0.2rem" }}>
+                            <Link href="https://github.com/manuelpalluotto/Projekt-Netzplan" target="_blank" rel="noopener noreferrer"><FaTimeline size={100} color={"#0f0f0f"} /></Link>
+                            <div className={styles.projectLink}>
+                                <span style={{ backgroundColor: "var(--youtubeFont)", color: "var(--ytBackground)", fontSize: "0.8rem" }}>
+                                    GitHub Repository
+                                </span>
+                                <h3 className={styles.projectTitle}>2. Netzplanerstellung (Projektmanagement)</h3>
+                            </div>
                         </div>
                         <p className={styles.projectDescription}>
-                            A Telegram bot that tracks and reports cryptocurrency prices from PancakeSwap, providing users with real-time price updates and notifications.
+                            Meine Java Anwendung zum Kalkulieren von Anfangs- und Endzeiten verschiedener Arbeitspakete, freier Puffer und Gesamtpuffer.
                         </p>
                         <div className={styles.projectFeatures}>
-                            <h4 className={styles.featuresTitle}>Key Features:</h4>
+                            <h4 className={styles.featuresTitle}>Zusammenfassung:</h4>
                             <ul className={styles.featuresList}>
-                                <li className={styles.featureItem}>Real-time price tracking from PancakeSwap DEX</li>
-                                <li className={styles.featureItem}>Telegram integration for instant notifications</li>
-                                <li className={styles.featureItem}>Customizable alerts for price movements</li>
-                                <li className={styles.featureItem}>Support for multiple tokens/pairs</li>
+                                <li className={styles.featureItem}>Ebenfalls grafische Oberfläche dank der Java Swing Bibliothek</li>
+                                <li className={styles.featureItem}>Eintragen der verschiedenen Arbeitspakete mit Dauer und vorhergehenden Arbeitspaketen</li>
+                                <li className={styles.featureItem}>Automatische Berechnung der spätesten und frühesten Anfangs- und Endzeitpunkte der Arbeitspakete</li>
+                                <li className={styles.featureItem}>Automatische Berechnung der freien- und Gesamtpuffer</li>
                             </ul>
                         </div>
                         <div className={styles.projectTech}>
-                            <h4 className={styles.techTitle}>Technologies Used:</h4>
-                            <p className={styles.techText}>Node.js, JavaScript, Telegram Bot API, Web3.js, PancakeSwap API</p>
+                            <h4 className={styles.techTitle}>Genutzte Technologien:</h4>
+                            <p className={styles.techText}>Java, Java Swing</p>
                         </div>
                     </div>
 
                     <div className={styles.project}>
-                        <h3 className={styles.projectTitle}>3. Solidity DAPP Game</h3>
-                        <div className={styles.projectLink}>
-                            <a href="https://github.com/manuelpalluotto/SolidityDappGame" target="_blank" rel="noopener noreferrer">
-                                GitHub Repository
-                            </a>
+                        <div style={{ margin: "0.2rem" }}>
+                            <Link href="https://github.com/manuelpalluotto/Spring_Boot_Learning_Journal_Backend" target="_blank" rel="noopener noreferrer"><BiLogoSpringBoot size={100} color={"#0f0f0f"} /></Link>
+                            <div className={styles.projectLink}>
+                                <span style={{ backgroundColor: "var(--youtubeFont)", color: "var(--ytBackground)", fontSize: "0.8rem" }}>
+                                    GitHub Repository
+                                </span>
+                                <h3 className={styles.projectTitle}>3. Learning Journal (Backend)</h3>
+                            </div>
                         </div>
                         <p className={styles.projectDescription}>
-                            A decentralized application (DApp) game built on Ethereum using Solidity smart contracts with a React frontend.
+                            Eine Java-Anwendung zum Erstellen und Abrufen verschiedener (Tagebuch-) Einträge bzw Notizen.<br /><br />
+                            Dieses Repository enthält reines Backend, da für das Frontend die React-Bibliothek statt Thymeleaf benutzt wurde und deshalb<br />
+                            separat gespeichert wurde.
                         </p>
                         <div className={styles.projectFeatures}>
-                            <h4 className={styles.featuresTitle}>Key Features:</h4>
+                            <h4 className={styles.featuresTitle}>Zusammenfassung:</h4>
                             <ul className={styles.featuresList}>
-                                <li className={styles.featureItem}>Smart contract-based game mechanics</li>
-                                <li className={styles.featureItem}>Ethereum blockchain integration</li>
-                                <li className={styles.featureItem}>Interactive web interface using React</li>
-                                <li className={styles.featureItem}>Metamask integration for transactions</li>
+                                <li className={styles.featureItem}>Kommunikation über RESTful APIs</li>
+                                <li className={styles.featureItem}>Authentifizierung über JWT-Token</li>
+                                <li className={styles.featureItem}>Nutzer können sich registrieren/ einloggen</li>
+                                <li className={styles.featureItem}>Nutzer können Einträge abrufen, erstellen und selbst erstellte Einträge bearbeiten</li>
                             </ul>
                         </div>
                         <div className={styles.projectTech}>
-                            <h4 className={styles.techTitle}>Technologies Used:</h4>
-                            <p className={styles.techText}>Solidity, React.js, Web3.js, Ethereum, Metamask</p>
+                            <h4 className={styles.techTitle}>Genutzte Technologien:</h4>
+                            <p className={styles.techText}>Java, Spring Boot, Spring Security, Spring Security JWT, Spring Email, MariaDB</p>
                         </div>
                     </div>
 
                     <div className={styles.project}>
-                        <h3 className={styles.projectTitle}>4. Laravel Auth API</h3>
-                        <div className={styles.projectLink}>
-                            <a href="https://github.com/manuelpalluotto/LaravelAuthApi" target="_blank" rel="noopener noreferrer">
-                                GitHub Repository
-                            </a>
+                        <strong>Hinweis: </strong>Da die letzten drei Projekte mit React/ Next.js entwickelt wurden und diese Bibliothek typischerweise<br/>
+                        in Single-Page-Applications eingesetzt wird, teilen sich diese drei Projekte ein Repository. Somit bitte nicht wundern,<br/> wenn die 
+                        Verlinkung auf jeweils die gleiche Page leitet.
+                    </div>
+
+                    <div className={styles.project}>
+                        <div style={{ margin: "0.2rem" }}>
+                            <Link href="https://github.com/manuelpalluotto/Learning_Journal_Frontend_Snake_FourWins" target="_blank" rel="noopener noreferrer"><FaReact size={100} color={"#0f0f0f"} /></Link>
+                            <div className={styles.projectLink}>
+                                <span style={{ backgroundColor: "var(--youtubeFont)", color: "var(--ytBackground)", fontSize: "0.8rem" }}>
+                                    GitHub Repository
+                                </span>
+                            </div>
                         </div>
+                        <h3 className={styles.projectTitle}>4. Learning Journal (Frontend)</h3>
                         <p className={styles.projectDescription}>
-                            A robust authentication API built with Laravel, providing secure user authentication endpoints for web applications.
+                            Das Gegenstück zum Eintrag darüber, somit das Frontend für die Anwendung zum Erstellen und Abrufen <br />
+                            von Einträgen.
+                            <br/><br/>
+                            Da die letzten drei Projekte mit React/Next.js entwickelt wurden und diese Frameworks hauptsächlich für 
+                            die Erstellung von Single-Page Applications (SPAs) genutzt werden, sind alle drei Projekte in einem gemeinsamen Repository organisiert.
                         </p>
                         <div className={styles.projectFeatures}>
-                            <h4 className={styles.featuresTitle}>Key Features:</h4>
+                            <h4 className={styles.featuresTitle}>Zusammenfassung:</h4>
                             <ul className={styles.featuresList}>
-                                <li className={styles.featureItem}>JWT-based authentication system</li>
-                                <li className={styles.featureItem}>User registration and login flows</li>
-                                <li className={styles.featureItem}>Password reset functionality</li>
-                                <li className={styles.featureItem}>Role-based access control</li>
+                                <li className={styles.featureItem}>Modulares und komponentenbasiertes UI mit React</li>
+                                <li className={styles.featureItem}>State-Management mit React Hooks</li>
+                                <li className={styles.featureItem}>Interaktion mit dem Backend über RESTful APIs</li>
+                                <li className={styles.featureItem}>Benutzerfreundliche Oberfläche für Authentifizierung und Eintragsverwaltung</li>
+                                <li className={styles.featureItem}>Styling mit CSS</li>
                             </ul>
                         </div>
                         <div className={styles.projectTech}>
-                            <h4 className={styles.techTitle}>Technologies Used:</h4>
-                            <p className={styles.techText}>PHP, Laravel, JWT, MySQL, RESTful API design</p>
+                            <h4 className={styles.techTitle}>Genutzte Technologien:</h4>
+                            <p className={styles.techText}>React Next.js mit TypeScript, Axios</p>
                         </div>
                     </div>
 
                     <div className={styles.project}>
+                        <div style={{ margin: "0.2rem" }}>
+                            <Link href="https://github.com/manuelpalluotto/Learning_Journal_Frontend_Snake_FourWins" target="_blank" rel="noopener noreferrer"><VscSnake size={100} color={"#0f0f0f"} /></Link>
+                            <div className={styles.projectLink}>
+                                <span style={{ backgroundColor: "var(--youtubeFont)", color: "var(--ytBackground)", fontSize: "0.8rem" }}>
+                                    GitHub Repository
+                                </span>
+                            </div>
+                        </div>
                         <h3 className={styles.projectTitle}>5. Spam Mail Detector</h3>
-                        <div className={styles.projectLink}>
-                            <a href="https://github.com/manuelpalluotto/spam-mail-detector" target="_blank" rel="noopener noreferrer">
-                                GitHub Repository
-                            </a>
-                        </div>
                         <p className={styles.projectDescription}>
                             A machine learning model implemented to detect and filter spam emails using natural language processing techniques.
                         </p>
